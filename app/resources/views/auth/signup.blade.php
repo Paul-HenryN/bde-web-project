0731ba15610f29693b1198e8d0dcde663c7bdb0b
ToukamSR <paul-henry.ngankam@2025.ucac-icam.com>
2022-05-07 15:17:23 +0100

    Authentication views ok

--- app/resources/views/auth/signup.blade.php
+++ app/resources/views/auth/signup.blade.php
@@ -1,45 +1,103 @@
-<section class="vh-100">
+@extends('templates.master')
+
+@section('styles')
+<link rel="stylesheet" href="{{asset('css/signup.css')}}">
+@endsection
+
+@section('content')
+<section class="test vh-100">
   <div class="container-fluid">
     <div class="row">
-      <div class="col-sm-6 text-black">
+      <div class="col-sm-6">
 
-        <div class="px-5 ms-xl-4">
+        <!-- <div class="px-5 ms-xl-4">
           <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
-          <span class="h1 fw-bold mb-0">Logo</span>
-        </div>
+        </div> -->
 
         <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
 
           <form style="width: 23rem;">
-
-            <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Log in</h3>
-
-            <div class="form-outline mb-4">
-              <input type="email" id="form2Example18" class="form-control form-control-lg" />
-              <label class="form-label" for="form2Example18">Email address</label>
+            <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+              <h1 class="">Join us</h1>
             </div>
 
-            <div class="form-outline mb-4">
-              <input type="password" id="form2Example28" class="form-control form-control-lg" />
-              <label class="form-label" for="form2Example28">Password</label>
+            <div class="divider d-flex align-items-center my-4"></div>
+
+            <div class="form-group mb-3">
+              <div class="form-outline">
+                <label class="form-label" for="surname">Fist name</label>
+                <input type="surname" id="surname" class="form-control ">
+              </div>
+
+              <div class="form-outline">
+                <label class="form-label" for="surname">Surname</label>
+                <input type="surname" id="surname" class="form-control ">
+              </div>
             </div>
 
-            <div class="pt-1 mb-4">
-              <button class="btn btn-info btn-lg btn-block" type="button">Login</button>
+
+            <div class="form-outline mb-3">
+              <label class="form-label" for="form2Example18">E-mail address</label>
+              <input type="email" id="email" class="form-control ">
             </div>
 
-            <p class="small mb-5 pb-lg-2"><a class="text-muted" href="#!">Forgot password?</a></p>
-            <p>Don't have an account? <a href="#!" class="link-info">Register here</a></p>
+            <div class="form-outline mb-3">
+              <label class="form-label" for="password">Password</label>
+              <input type="password" id="password" class="form-control " />
+            </div>
+
+            <div class="form-outline mb-3">
+              <label class="form-label" for="password_confirm">Confirm your password</label>
+              <input type="password" id="password_confirm" class="form-control " />
+            </div>
+
+            <div class="form-outline mb-3">
+              <label class="form-label" for="password_confirm">Location</label>
+              <select class="form-select" name="location" id="location">
+                <option value="">CESI Douala</option>
+                <option value="">CESI Rouen</option>
+                <option value="">CESI Paris</option>
+                <option value="">CESI Montpellier</option>
+              </select>
+            </div>
+
+
+            <div class="mb-4">
+              <label for="formFile" class="form-label">Avatar</label>
+              <input class="form-control" type="file" id="formFile" accept=".jpg,.png,.gif">
+            </div>
+
+            <!-- Checkbox -->
+            <div class="form-check mb-0">
+              <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
+              <label class="form-check-label" for="form2Example3">
+                I'have read and accepted the <a href="">Terms and privacy policy.</a>
+              </label>
+            </div>
+
+            <!-- Checkbox -->
+            <div class="form-check mb-4">
+              <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
+              <label class="form-check-label" for="form2Example3">
+                Receive update via e-mail about new events.
+              </label>
+            </div>
+
+            <div class="pt-1 mb-3">
+              <button type="button" class="btn btn-primary btn-lg">Sign up</button>
+            </div>
+
+            <p class="fw-bold">Already registered ? <a href="/login" class="link-info">Login into your account</a></p>
 
           </form>
 
         </div>
 
       </div>
-      <div class="col-sm-6 px-0 d-none d-sm-block">
-        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img3.webp"
-          alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
+      <div class="col-sm-6 px-0 d-none d-md-block">
+        <img src="{{asset('images/summary.jpg')}}" alt="Ucac-icam site" class="hero w-100" style="object-fit: cover; object-position: left;">
       </div>
     </div>
   </div>
 </section>
+@endsection
